03fc9617af2587ed0d2e8cf59c3e90080de52284
Philip <hyngsuki@gmail.com>
2019-11-09 14:33:07 -0500

    Part2- scene settings are created

---
+++ Assets/_Scripts/SceneSettings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "SceneSettings", menuName ="Scene/Settings")]
+[System.Serializable]
+
+public class SceneSettings : ScriptableObject
+{
+    [Header("Scene Configuration")]
+    public SoundClip activeSoundClip;
+
+    [Header("Scoreboard ")]
+    public bool scoreLabelEnabled;
+     public bool livesLabelEnabled;
+     public bool highScoreLabelEnabled;
+
+    [Header("Scene Labels")]
+    public bool startLabelActive;
+    public bool endLabelSetActive;
+
+
+    [Header("Scene buttons")]
+    public bool startButtonActive;
+    public bool restartButtonActive;
+
+}
